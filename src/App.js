6302c97e9bdb6fc8f85daacc5652fe3c335b9bfc
Cotidib <constanza.dibueno@gmail.com>
2021-08-24 13:00:31 -0300

    add filters

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   }, []);
 
   const handleRemove = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id);
+    let newTours = tours.filter((tour) => tour.id !== id);
     setTours(newTours);
   }
 
@@ -34,6 +34,30 @@ function App() {
         <Loading />
       </main>
     );
+  }
+
+  const handleOneWeek = () => {
+    let newTours = tours.filter((tour) => 
+    parseInt(tour.name.match(/\d+/g)) <= 7
+    );
+    setTours(newTours);
+  }
+
+  const handleTwoWeek = () => {
+    let newTours = tours.filter((tour) => 
+    parseInt(tour.name.match(/\d+/g)) > 7 && parseInt(tour.name.match(/\d+/g)) <= 14
+    );
+    setTours(newTours);
+  }
+
+  const handleSecondPrice = () => {
+    let newTours = tours.filter((tour) => parseInt(tour.price.replace(",","")) < 3000);
+    setTours(newTours);
+  }
+
+  const handleThirdPrice = () => {
+    let newTours = tours.filter((tour) => parseInt(tour.price.replace(",","")) < 4000 && parseInt(tour.price.replace(",","")) > 3000);
+    setTours(newTours);
   }
   
   if(tours.length === 0)
@@ -47,6 +71,14 @@ function App() {
     
   return (
     <main>
+      {/* <header className='tours-header'>Our Tours <FontAwesomeIcon icon={faGlobe}/></header> */}
+      <div className='filters'>
+        <button className='filter-btn' onClick={fetchData}>All</button>
+        <button className='filter-btn' onClick={handleOneWeek}>1 week tours</button>
+        <button className='filter-btn' onClick={handleTwoWeek}>1-2 weeks tours</button>
+        <button className='filter-btn' onClick={handleSecondPrice}>$2,000</button>
+        <button className='filter-btn' onClick={handleThirdPrice}>$3,000</button>
+      </div>
       <ToursList tours={tours} handleRemove={handleRemove}/>
     </main>
   );
--- src/ToursList.js
+++ src/ToursList.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Tour from './Tour';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+
 
 const ToursList = (props) => {
     return (
         <section>
-            <header className='tours-header'>Tours <FontAwesomeIcon icon={faGlobe}/></header>
             <div className='list'>
                 {
                     props.tours.map((tour) => {
